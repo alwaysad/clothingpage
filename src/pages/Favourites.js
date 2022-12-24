2b267d8de11d062a356b7f0f8f10683cdff6fa8b
Ozkan Turidi <73255665+alwaysad@users.noreply.github.com>
2022-12-24 22:56:39 +0100

    Update Favourites.js

--- src/pages/Favourites.js
+++ src/pages/Favourites.js
@@ -7,7 +7,7 @@ const Favourites=()=>{
     const Items=useSelector((state)=>state.favourite.items);
     
     return <div>
-        {!Items.length&&<div className="flex justify-center content-center"><p class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">There is no item in favourites</p></div>}
+        {!Items.length&&<div className="flex justify-center min-h-screen items-center"><p class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">There is no item in favourites</p></div>}
         {Items.length&&<ul className="flex justify-center space-y-6">
          <li>
             {Items.map(item=>(
